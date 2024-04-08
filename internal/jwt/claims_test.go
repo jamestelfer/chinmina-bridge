3bb9dfcc06dc6561ee2b6f55232dab863270ec0a
James <792299+jamestelfer@users.noreply.github.com>
2024-04-08 18:15:35 +1000

    Add tests for custom claim validation

---
+++ internal/jwt/claims_test.go
@@ -0,0 +1,59 @@
+package jwt
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildkiteClaims_Validate(t *testing.T) {
+	t.Run("valid", func(t *testing.T) {
+		claims := &BuildkiteClaims{
+			Audience:                 "audience",
+			OrganizationSlug:         "org",
+			PipelineSlug:             "pipeline",
+			BuildNumber:              "123",
+			BuildBranch:              "main",
+			BuildCommit:              "abc123",
+			StepKey:                  "step1",
+			JobId:                    "job1",
+			AgentId:                  "agent1",
+			expectedOrganizationSlug: "org",
+		}
+
+		err := claims.Validate(context.Background())
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("missing claims", func(t *testing.T) {
+		claims := &BuildkiteClaims{}
+
+		err := claims.Validate(context.Background())
+
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "missing expected claim(s)")
+	})
+
+	t.Run("wrong org", func(t *testing.T) {
+		claims := &BuildkiteClaims{
+			Audience:     "audience",
+			PipelineSlug: "pipeline",
+			BuildNumber:  "123",
+			BuildBranch:  "main",
+			BuildCommit:  "abc123",
+			StepKey:      "step1",
+			JobId:        "job1",
+			AgentId:      "agent1",
+
+			OrganizationSlug:         "wrong",
+			expectedOrganizationSlug: "right",
+		}
+
+		err := claims.Validate(context.Background())
+
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "expecting token issued for organization")
+	})
+}
