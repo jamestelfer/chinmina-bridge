63c99f4f521491afe6092cc4c2c24dcf9588addf
James <792299+jamestelfer@users.noreply.github.com>
2024-04-09 23:17:36 +1000

    Make the JWT utility a little less env specific

--- cmd/create/main.go
+++ cmd/create/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"time"
 
 	localjwt "github.com/jamestelfer/ghauth/internal/jwt"
+	"github.com/sethvargo/go-envconfig"
 	"gopkg.in/go-jose/go-jose.v2"
 	"gopkg.in/go-jose/go-jose.v2/json"
 	"gopkg.in/go-jose/go-jose.v2/jwt"
 )
 
+type Config struct {
+	Audience         string `env:"UTIL_AUDIENCE, default=test-audience"`
+	Subject          string `env:"UTIL_SUBJECT, default=test-subject"`
+	Issuer           string `env:"UTIL_ISSUER, default=https://local.testing"`
+	OrganizationSlug string `env:"UTIL_BUILDKITE_ORGANIZATION_SLUG, required"`
+	PipelineSlug     string `env:"UTIL_BUILDKITE_PIPELINE_SLUG, required"`
+}
+
 func main() {
+	cfg := Config{}
+	err := envconfig.Process(context.Background(), &cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading config: %v\n", err)
+		os.Exit(1)
+	}
+
 	jwksPath := ".development/keys/jwks.private.json"
 
 	jwksBytes, err := os.ReadFile(jwksPath)
@@ -30,12 +47,12 @@ func main() {
 	key := jwks.Key("test-key")[0]
 
 	jwt, err := createJWT(&key, validity(jwt.Claims{
-		Audience: []string{"test-audience"},
-		Subject:  "subject",
-		Issuer:   "https://local.testing",
+		Audience: []string{cfg.Audience},
+		Subject:  cfg.Subject,
+		Issuer:   cfg.Issuer,
 	}), localjwt.BuildkiteClaims{
-		OrganizationSlug: "test-org",
-		PipelineSlug:     "test-pipeline",
+		OrganizationSlug: cfg.OrganizationSlug,
+		PipelineSlug:     cfg.PipelineSlug,
 		BuildNumber:      "123",
 		BuildBranch:      "main",
 		BuildCommit:      "abc123",
